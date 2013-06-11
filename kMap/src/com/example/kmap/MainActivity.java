d390a0d1ed62a3bb35d0fd7b91e2980ddab15f7b
kennyhouse <kennyhouse@gmail.com>
2013-06-10 21:54:44 -0700

    change.

--- kMap/src/com/example/kmap/MainActivity.java
+++ kMap/src/com/example/kmap/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	//comment
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
